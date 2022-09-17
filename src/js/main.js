22b1fa3d3dd4766e220a3f906d858c5768846a3c
qipros_ <titan.2021@mail.ru>
2022-09-17 13:31:13 +0600

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+
+
+let store = document.querySelector('.store__hide-card')
+let button2 = document.querySelector('.storeButton')
+
+button2.addEventListener('click', function () {
+    store.style.display = 'flex', button2.style.display = 'none'
+})
